ff56ac06ff0597cc9e70f7f45262e03078764184
EvaMicich <6366@holbertonstudents.com>
2023-09-02 04:44:54 -0700

    annotated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Yield random numbers between 0 and 10 after waiting for 1 second."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield random numbers between 0 and 10 after waiting for 1 second."""
     for _ in range(10):
         await asyncio.sleep(1)
