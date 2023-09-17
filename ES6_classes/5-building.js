2fe0847d047305c8c2ee2f34503f1dcf0753e1e5
EvaMicich <6366@holbertonstudents.com>
2023-09-17 09:47:51 +0000

    building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+class Building {
+    constructor(sqft) {
+      if (this.constructor === Building) {
+        throw new Error('Building is an abstract class and cannot be instantiated directly.');
+      }
+      if (typeof sqft !== 'number') {
+        throw new Error('Sqft must be a number.');
+      }
+      this._sqft = sqft;
+
+      // Check if the extending class has the method 'evacuationWarningMessage'
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+
+    get sqft() {
+      return this._sqft;
+    }
+  }
+
+  export default Building;
