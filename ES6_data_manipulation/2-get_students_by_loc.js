1596a43b9e70f3697e4e7fb2115a9707add4d711
EvaMicich <6366@holbertonstudents.com>
2023-10-08 15:47:35 +1100

    student by loc

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,4 @@
+export default function getStudentsByLocation (listOfStudents, city) {
+  const result = listOfStudents.filter((listOfStudents) => listOfStudents.location === city);
+  return result;
+}
