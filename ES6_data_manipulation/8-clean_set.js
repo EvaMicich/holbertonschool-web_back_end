c278060ac9d29d1653cf8f909a75d83648effbc1
EvaMicich <6366@holbertonstudents.com>
2023-10-10 11:16:53 +1100

    clean

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  let result = [];
+  const result = [];
   for (const value of set) {
-    if (value.startsWith(startString) && value !== undefined) {
+    if (value !== undefined && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }
